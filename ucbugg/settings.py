2fb60420d88217282a6756253790bc9cca20565c
Derferman <kyle.j.conroy@gmail.com>
2010-11-30 19:06:41 -0800

    Fixed broken path

--- ucbugg/settings.py
+++ ucbugg/settings.py
@@ -1,10 +1,10 @@
 import os
 
 #Stashboard version
-VERSION = "0.1.5"
+VERSION = "0.1.6"
 
 # Add utils and templates to the system path
 APP_ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
 TEMPLATE_DIR = os.path.join(APP_ROOT_DIR, "templates")
-CHAPTERS_DIR = os.path.join(MARKDOWN_DIR, "chapters")
-TUTORIAL_DIR = os.path.join(MARKDOWN_DIR, "tutorials")
+CHAPTERS_DIR = os.path.join(APP_ROOT_DIR, "chapters")
+TUTORIAL_DIR = os.path.join(APP_ROOT_DIR, "tutorials")
